66551ac23fe88acd34fa450bebef24abd1d84827
Louis <vanaaardtlj03@gmail.com>
2025-04-24 15:22:31 +0200

    added validate chirp function

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
@@ -20,6 +22,7 @@ func main() {
 	mux.Handle("GET /api/healthz", apiCfg.middlewareMetricsInc(http.HandlerFunc(readiness)))
 	mux.Handle("GET /admin/metrics", apiCfg.middlewareMetricsInc(http.HandlerFunc(metrics)))
 	mux.Handle("POST /admin/reset", http.HandlerFunc(reset))
+	mux.Handle("POST /api/validate_chirp", http.HandlerFunc(validate_chirp))
 
 	server := &http.Server{Handler: mux, Addr: ":8080"}
 	server.ListenAndServe()
@@ -36,6 +39,51 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 		cfg.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func validate_chirp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	type chirp struct {
+		Body string `json:"body"`
+	}
+	type return_val struct {
+		Body  string `json:"body"`
+		Err   string `json:"error"`
+		Valid string `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	chirp_body := chirp{}
+	err := decoder.Decode(&chirp_body)
+	if err != nil {
+		log.Printf("Error decoding parameters: %v", err)
+		w.WriteHeader(500)
+		return
+	}
+	if len(chirp_body.Body) > 140 {
+		respBody := return_val{
+			Body: chirp_body.Body,
+		}
+		data, err := json.Marshal(respBody)
+		if err != nil {
+			log.Printf("Error marshaling data: %v", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.WriteHeader(200)
+		w.Write(data)
+		return
+	}
+	respBody := return_val{Err: "Chirp is too long"}
+	data, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshaling data: %v", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(500)
+	w.Write(data)
+
 }
 
 func metrics(w http.ResponseWriter, r *http.Request) {
